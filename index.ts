41314979c3eb7c7239b609bd64cb89ffcd804188
Alexander Kaufmann <ak@vivid-planet.com>
2018-10-31 18:04:42 +0100

    parse domain from loaded scripttag and set it as webpack public path

--- index.ts
+++ index.ts
@@ -1,6 +1,21 @@
 import * as app from "./src/app";
 
+declare let __webpack_public_path__: string;
+
 const loadHtml = () => {
+    let domain = null;
+    const scripts = document.getElementsByTagName("script");
+    [].every.call(scripts, (script: HTMLScriptElement) => {
+        const m = script.src.match(/^(.*)?\/assets\/build\/kwf-react-starter/);
+        if (m) {
+            domain = m[1];
+            return false;
+        }
+        return true;
+    });
+    if (domain === null) return false;
+    __webpack_public_path__ = domain + __webpack_public_path__;
+
     const baseEl: HTMLElement = document.querySelector("kwf-react-starter");
     if (!baseEl) return false;
 
