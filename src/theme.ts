2acc867e7621fe2b82831e1ffb2141edefcece39
Alexander Kaufmann <ak@vivid-planet.com>
2019-02-06 07:33:18 +0100

    fix strict null checks in theme

--- src/theme.ts
+++ src/theme.ts
@@ -25,11 +25,11 @@ export const theme: ITheme = {
 };
 
 export const getColor = (color: keyof IColors): ((props: IWithTheme) => string) => {
-    return ({ theme: { colors } }) => colors[color];
+    return ({ theme: t }) => t!.colors[color];
 };
 
 export const getBreakpoint = (breakpoint: keyof IBreakpoints): ((props: IWithTheme) => string) => {
-    return ({ theme: { breakpoints } }) => breakpoints[breakpoint];
+    return ({ theme: t }) => t!.breakpoints[breakpoint];
 };
 
 export const isMobile = (): boolean => window.innerWidth < 1024;
